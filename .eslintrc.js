2c0d922b3a31a274c172a20944b62d667d800383
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-09-28 15:30:57 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+    extends: ["eslint:recommended", "prettier" ],
+    overrides: [
+      {
+        env: {
+          node: true,
+        },
+        files: [".eslintrc.{js,cjs}"],
+        parserOptions: {
+          sourceType: "script",
+        },
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    parserOptions: {
+      ecmaVersion: "latest",
     },
-    "rules": {
-    }
-}
+    rules: {
+      indent: ["error", 2, { SwitchCase: 1 }],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+    },
+  };
+  

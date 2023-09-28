aaad1e5f9b5b63251c262e78f44152ca237a3cb0
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-09-28 00:56:42 -0700

    Created and styled register page.

---
+++ public/js/register.js
@@ -0,0 +1,4 @@
+/*Client-side validation using JavaScript to ensure that users provide valid 
+and complete information. Validate email formats, password strength, and check 
+if the password and confirm password fields match. Before sending data to server*/
+

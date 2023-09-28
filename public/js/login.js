0be75f95cd6c894db6e8487f494bb7fcd82d7a75
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-09-28 10:01:42 -0700

    s

---
+++ public/js/login.js
@@ -0,0 +1,4 @@
+/*Client-side validation using JavaScript to ensure that users provide valid 
+and complete information. Validate email/username format and password length 
+before sending data to server*/
+

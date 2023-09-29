48de1dcdac829c5d44c28a32849e0e57b36b5401
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-09-28 20:04:59 -0700

    Integrated pages before logging in.

--- public/js/login.js
+++
@@ -1,7 +0,0 @@
-/*Client-side validation using JavaScript to ensure that users provide valid 
-and complete information. Validate email/username formats, as well aspassword 
-strength Before sending data to server*/
-
-document.addEventListener('DOMContentLoaded', function () {
-    return
-});

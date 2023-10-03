bc138c48a921ef7f1864c8aba909a2a397b9059f
Zhiqian-Zhang <zhiqianz29@gmail.com>
2023-10-03 10:48:56 -0700

    fix issues based on ESLint

--- models/users.js
+++ models/users.js
@@ -2,9 +2,9 @@
 Update, Delete) operations for managing user data in MongoDB. It also includes functions for 
 password hashing and password verification.*/
 
-const { ObjectId } = require('mongodb');
-const { getDatabase } = require('../db/db');
-const bcrypt = require('bcrypt');
+const { ObjectId } = require("mongodb");
+const { getDatabase } = require("../db/db");
+const bcrypt = require("bcrypt");
 
 // Define the User schema
 const userSchema = {
@@ -26,39 +26,40 @@ const UserModel = {
       password: hashedPassword,
     };
 
-    const result = await db.collection('users').insertOne(newUser);
+    const result = await db.collection("users").insertOne(newUser);
     return result.insertedId;
   },
 
   async getUserByUsername(username) {
     const db = getDatabase();
-    return db.collection('users').findOne({ username });
+    return db.collection("users").findOne({ username });
   },
 
   async getUserByEmail(email) {
     const db = getDatabase();
-    return db.collection('users').findOne({ email });
+    return db.collection("users").findOne({ email });
   },
 
   async updateUser(id, updatedUser) {
     const db = getDatabase();
-    const result = await db.collection('users').updateOne(
-      { _id: ObjectId(id) },
-      { $set: updatedUser }
-    );
+    const result = await db
+      .collection("users")
+      .updateOne({ _id: ObjectId(id) }, { $set: updatedUser });
 
     return result.modifiedCount;
   },
 
   async deleteUser(id) {
     const db = getDatabase();
-    const result = await db.collection('users').deleteOne({ _id: ObjectId(id) });
+    const result = await db
+      .collection("users")
+      .deleteOne({ _id: ObjectId(id) });
     return result.deletedCount;
   },
 
   async verifyPassword(email, password) {
     const db = getDatabase();
-    const user = await db.collection('users').findOne({ email });
+    const user = await db.collection("users").findOne({ email });
 
     if (!user) {
       return false; // User not found

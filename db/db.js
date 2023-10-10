42f1305c317d292ebcd20315c08b4491049f8d44
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-10 16:02:51 -0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,26 +1,33 @@
-// db.js
-
 const { MongoClient } = require("mongodb");
-
+// Replace the uri string with your connection string.
+const uri = "mongodb://127.0.0.1:27017";
 const dbName = "MentalHealthMonitor";
-
-const url = `mongodb://localhost:27017/${dbName}`;
-const client = new MongoClient(url);
-
+const client = new MongoClient(uri);
+let database;
 async function connectToDatabase() {
+  //console.log("Mongo connecting to", uri);
   try {
-    await client.connect();
+    database = client.db(dbName);
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
+    console.log("Error with db connection", error);
   }
 }
-
+// // db.js// const { MongoClient } = require("mongodb");// const dbName = "MentalHealthMonitor";// const url = `mongodb://localhost:27017`;
+// const client = new MongoClient(url);// async function connectToDatabase() {
+//   try {
+//     console.log("Connecting to ", url);
+//     await client.connect();
+//     console.log("Connected to MongoDB");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//   }
+// }
 function getDatabase() {
-  return client.db(dbName);
+  return database;
 }
-
 module.exports = {
   connectToDatabase,
   getDatabase,
 };
+// connectToDatabase();

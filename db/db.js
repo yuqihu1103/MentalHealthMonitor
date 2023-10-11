955e96e97f34fa2a436dd8bb08a1fa2d3f690ba2
yuqihu1103 <hu.yuqi@northeastern.edu>
2023-10-11 01:08:40 -0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = "mongodb://127.0.0.1:27017";
+const uri = "mongodb://localhost:27017";
 const dbName = "MentalHealthMonitor";
 const client = new MongoClient(uri);
 
